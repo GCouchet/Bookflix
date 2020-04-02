dc1ad899784bbcf0f4de1cae6c4f22eb7f7489fa
fermin10 <fer.lg.2016@gmail.com>
2020-04-01 23:11:53 -0300

    Creación y selección de perfiles. Se guarda en request.session.get['myProfile'] el id del perfil seleccionado. (falta eliminar perfil y limite de perfiles por cuenta)

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
-from .forms import UCFWithExtends
-
+from .forms import UCFWithExtends, creationProfile
+from .models import Profile
 
 def register(request):
     """Register a new user"""
